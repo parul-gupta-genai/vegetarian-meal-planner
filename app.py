1a3c8dc8861603e614543d037d58f1a822d3b8d5
Parul Gupta <pg60000000@gmail.com>
2026-07-01 11:52:32 +0000

    Configure Flask for Cloud Run

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 import requests
 import math
@@ -388,4 +389,4 @@ def generate_plan():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
